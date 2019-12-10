d08c6a2efba7a6a2df3cea3cf4aa707ca375fa3d
Jacob Harrington <jharrington4@gmail.com>
2019-12-10 14:56:55 -0800

    finished get all attempts route and tests passing

--- __tests__/attempt.test.js
+++ __tests__/attempt.test.js
@@ -32,10 +32,33 @@ describe('attempt routes', () => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           recipeId: 5,
-          dateOfEvent: expect.any(String),
+          dateOfEvent: new Date('2019-12-10').toISOString(),
           notes: 'this is a note',
           rating: 3,
           __v: 0
+        });
+      });
+  });
+
+  it('gets all attempts', async() => {
+    const attempts = await Attempt.create([
+      { recipeId: 5, dateOfEvent: '2019-12-10', notes: 'this is a note', rating: 3 },
+      { recipeId: 6, dateOfEvent: '2019-12-09', notes: 'this is a note', rating: 2 },
+      { recipeId: 7, dateOfEvent: '2019-12-08', notes: 'this is a note', rating: 1 },
+    ]);
+
+    return request(app)
+      .get('/api/v1/attempts')
+      .then(res => {
+        attempts.forEach(attempt => {
+          expect(res.body).toContainEqual({
+            _id: attempt._id.toString(),
+            recipeId: attempt.recipeId,
+            dateOfEvent: attempt.dateOfEvent.toISOString(),
+            notes: attempt.notes,
+            rating: attempt.rating,
+            __v: 0
+          });
         });
       });
   });
